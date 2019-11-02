2ec5f17c3481af42635c6cda434ac622c4925308
zaxwebs <zackwebs1000@gamil.com>
2019-11-02 16:46:34 +0530

    Added GET and POST helpers

--- d-core/general.php
+++ d-core/general.php
@@ -5,3 +5,13 @@ function redirect(string $url = '')
     header('Location: /' . $url);
     exit;
 }
+
+function GET(string $key, $default = null)
+{
+    return isset($_GET[$key]) ? $_GET[$key] : $default;
+}
+
+function POST(string $key, $default = null)
+{
+    return isset($_POST[$key]) ? $_POST[$key] : $default;
+}
